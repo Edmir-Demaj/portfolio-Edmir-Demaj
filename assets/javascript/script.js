a34248163dce0d13a183a314a8d5619e4cad210c
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-03-17 21:07:36 +0000

    Make contact form valid by submitting form to my email

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    // add class active to home page
-    document.getElementById("homepage").classList.add("active");
+  // add class active to home page
+  document.getElementById("homepage").classList.add("active");
 });
 
 // Get all elements with class="nav-link"
@@ -9,9 +8,9 @@ let navLinks = document.getElementsByClassName("nav-link");
 
 // Loop through the nav-links and add the active class
 for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", function () {
-        let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
+  navLinks[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
